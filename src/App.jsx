c05ab4d76385f3ba942bb29893cb7367305ba0e8
rejennylo <rejennylo@gmail.com>
2023-11-15 18:32:59 +0800

    feat: add Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { cards } from './datas';
+import { activityCards } from './datas';
+import { issueCards } from './datas';
 import { Activity } from './components/Activity';
 import { Banner } from './components/Banner';
 import { Header } from './components/Header';
+import { Issue } from './components/Issue';
 
 function App() {
   return (
     <div className="border-x-[5px] md:border-t-[5px]  border-main-purple">
       <Header />
       <Banner />
-      <Activity cards={cards} />
+      <Activity cards={activityCards} />
+      <Issue cards={issueCards} />
     </div>
   );
 }
---
+++ src/components/Issue.jsx
@@ -0,0 +1,43 @@
+const Card = ({ card, cardClass }) => {
+  return (
+    <div
+      name="card"
+      className={`h-[320px] w-[330px] lg:h-[366px] lg:w-[379px] rounded-xl bg-white p-3 mb-5 lg:mb-0 ${cardClass}`}
+    >
+      <span className="inline-block text-white bg-secondary-green text-sm px-2 py-1 mb-2 rounded-tl-xl rounded-br-xl">
+        {card.number}
+      </span>
+      <h5>{card.title}</h5>
+      <ul>
+        <li>
+          <p>{card.text1}</p>
+        </li>
+        <li>
+          <p>{card.text2}</p>
+        </li>
+        <li>
+          <p>{card.text3}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export const Issue = ({ cards }) => {
+  return (
+    <div name="issue-wrap" className="bg-main-purple">
+      <div
+        name="issue-container"
+        className="m-auto max-w-[1200px] lg:h-[578px] flex flex-col items-center"
+      >
+        <h2 className="text-white my-5">政策議題</h2>
+        <div className="lg:h-[396px] lg:flex lg:justify-between lg:w-full gap-5">
+          {cards.map((card, i) => {
+            const selfEnd = i === 1 && 'lg:self-end';
+            return <Card key={i} card={card} cardClass={selfEnd} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
